13006138e5682cd2f1480e4d05cd2169efad5980
ikiakmal15 <167083527+ikiakmal15@users.noreply.github.com>
2024-10-12 12:26:00 +0700

    Add files via upload

---
+++ ddp.py
@@ -0,0 +1,7 @@
+print()
+print('=== tabung===')
+r = 7
+tinggi = 60
+phi =3.14
+rumus = (2*phi*r*tinggi)
+print('keliling tabung adalah', rumus)
---
+++ tbideal.py
@@ -0,0 +1,8 @@
+print()
+print('===Badan Ideal===')
+
+tinggi_badan = 158
+berat_badan_ideal = (tinggi_badan-100)-(tinggi_badan-100)*1/10
+
+print('Berat badan ideal untuk tinggi', tinggi_badan,
+        'CM adalah', berat_badan_ideal, 'kg')
